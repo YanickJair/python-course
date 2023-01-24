b557645d05971d9dccfc0ef6788cf34c83b2241f
yannick <yanick.jair.ta@gmail.com>
2023-01-24 00:22:55 +0000

    working with strings

--- src/topics/data_types.py
+++ src/topics/data_types.py
@@ -40,6 +40,7 @@ def string():
     Em Python existe 3 formas mais comuns de se fazer e vamos vê-las agora.
     """
     )
+
     st.markdown(
         """
         #### A primeira forma de Concatenar
@@ -51,5 +52,51 @@ def string():
         """primeiro_nome = 'Yanick'\napelido = 'Andrade'\nnome_completo = primeiro_nome + apelido
     """
     )
+    st.code("print(nome_completo)")
     st.text("Resultado")
     st.code("YanickAndrade", language="sh")
+
+    st.markdown(""" 
+    Reparaste que o **Primeiro Nome** e o **Apelido** ficaram 'colados'?
+    Pois. Podemos resolver isso concatenando mais uma **string** em branco no meio das duas.
+    """)
+
+    st.code(""" 
+    nome_completo = primeiro_nome + ' ' + apelido
+    """)
+    st.code("print(nome_completo)")
+    st.text("Resultado")
+    st.code("Yanick Andrade", language="sh")
+
+
+    st.markdown(
+        """
+        #### A segunda forma de Concatenar
+
+        A segunda forma de concatenar é usando **f-string**, que é o abreviativo de **Formatted string literals**.
+
+        F-strings dá-nos uma forma de incorporar uma string dentro de uma outra string/texto 
+        usando uma sintaxe mais flexivel e minimalista.
+    """
+    )
+    st.code(
+        """primeiro_nome = 'Yanick'\napelido = 'Andrade'\nnome_completo = f'{primeiro_nome} {apelido}
+    """
+    )
+    st.code("print(nome_completo)")
+    st.text("Resultado")
+    st.code("Yanick Andrade", language="sh")
+
+    st.markdown(""" 
+    Outra forma de usar o f-strings por exemplo é escrevendo um texto e usar a variavél com o nome
+    para fazer o *auto-complete*.
+    """)
+    st.code("print(f'Olá, meu nome é {nome_completo}')")
+    st.text("Resultado")
+    st.code("Olá, meu nome é Yanick Andrade", language="sh")
+
+
+
+
+
+    
